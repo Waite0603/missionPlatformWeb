84269fe93f2854d471e6a8cfd7ed3f7ac88b2587
Waite Wang <waite@waite.wang>
2024-08-31 14:59:46 +0800

    🐎 ci: 实现自动导入组件

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import path from 'path'
+import { PrimeVueResolver } from '@primevue/auto-import-resolver'
+
+import Components from 'unplugin-vue-components/vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [
+		vue(),
+		Components({
+			resolvers: [PrimeVueResolver()]
+		})
+	],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src')
